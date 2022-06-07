3fcfb6e6da52be4fe94eb68dbd5dba26e3b54db4
zjkwdy <3377911508@qq.com>
2022-06-07 23:13:45 +0800

    format & fixbug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-$(document).ready(() =>
-{
-    root=$('body');
-    video=$('#background')[0];
+$(document).ready(() => {
+    root = $('body');
+    video = $('#background')[0];
     // src: Foxtail-Grass Studio - 歳月-雲流れ- (岁月-云流-)
     bgm = new Audio('assets/res/bgm.mp3');
     bgm.load();
@@ -10,11 +9,11 @@ $(document).ready(() =>
     //     /* 音频可以播放；如果权限允许则播放 */
     //     bgm.play();
     // });
-    root.click(function (){
+    root.click(function () {
         // video.muted=false;
         video.play();
-        bgm.loop=true;
-        bgm.autoplay=true;
+        bgm.loop = true;
+        bgm.autoplay = true;
         bgm.play();
     })
 });
