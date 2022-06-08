5c0a85164558af8463cbdaf1b7c0c517f9472a02
zjkwdy <3377911508@qq.com>
2022-06-08 13:18:08 +0800

    text anim

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,13 @@ $(document).ready(() => {
     //     /* 音频可以播放；如果权限允许则播放 */
     //     bgm.play();
     // });
+    dom_tit=$('#mtitle')[0];
+    dom_moe=$('#Moe')[0];
+    // console.log(dom_tit,dom_moe)
+    video.addEventListener('play', function (){
+        dom_tit.style.animationPlayState='running';
+        dom_moe.style.animationPlayState='running';
+    })
     root.click(function () {
         // video.muted=false;
         video.play();
