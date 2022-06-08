ccbecd05fb5b889be5e9e0744e3e46c88e270e80
zjkwdy <3377911508@qq.com>
2022-06-08 17:51:28 +0800

    text clo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,11 @@ $(document).ready(() => {
     // src: Foxtail-Grass Studio - 歳月-雲流れ- (岁月-云流-)
     bgm = new Audio('assets/res/bgm.mp3');
     bgm.load();
-    // Chrome不允许自动播放了(悲)
-    // bgm.addEventListener("canplaythrough", event => {
-    //     /* 音频可以播放；如果权限允许则播放 */
-    //     bgm.play();
-    // });
+    // Chrome不允许自动播放了,不过还是要试一下
+    bgm.addEventListener("canplaythrough", event => {
+        /* 音频可以播放；如果权限允许则播放 */
+        bgm.play();
+    });
     dom_tit=$('#mtitle')[0];
     dom_moe=$('#Moe')[0];
     // console.log(dom_tit,dom_moe)
